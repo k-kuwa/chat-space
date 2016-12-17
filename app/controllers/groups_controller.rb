d3ff605e1cfe2919175ab0f415b621b582810d79
k-kuwa <kyosuke@Kyosuke-no-MacBook-Pro.local>
2016-12-17 19:43:00 +0900

    Refactoring

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,13 +7,10 @@ class GroupsController < ApplicationController
 
  def create
   @group = Group.create(group_params)
-
  end
 
-private
-
+ private
  def group_params
-  params.require(:group).permit(:name, {:user_ids => []})
+  params.require(:group).permit(:name, {user_ids: []})
  end
-
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,7 @@
 class Group < ApplicationRecord
-
   validates :name, presence: true
   has_many :messages
   has_many :users, through: :group_users
   has_many :group_users
-
   accepts_nested_attributes_for :group_users
-
 end
