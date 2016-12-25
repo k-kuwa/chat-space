aa8549327dd39cf86c4a937e6030291db9add5a2
k-kuwa <kyosuke@Kyosuke-no-MacBook-Pro.local>
2016-12-25 13:51:31 +0900

    Correcting the code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,15 +12,12 @@ class MessagesController < ApplicationController
   def create
     @message = Message.create(message_params)
     redirect_to group_messages_path(params[:group_id])
-   if @message.save
-   else
-      flash[:alert] =  'メッセージを入力してください'
-   end
+    flash[:alert] = 'メッセージを入力してください' unless @message.save
   end
 
  private
   def message_params
-     params.require(:message).permit(:body, :image).merge(user_id:current_user.id ,group_id: params[:group_id])
+    params.require(:message).permit(:body, :image).merge(user_id:current_user.id ,group_id: params[:group_id])
   end
 
 end
