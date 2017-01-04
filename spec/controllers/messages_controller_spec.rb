2e3602fe7c95958cc0c60479ae3977c8880699f7
k-kuwa <kyosuke@Kyosuke-no-MacBook-Pro.local>
2017-01-04 14:11:53 +0900

    Delete space

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -11,18 +11,18 @@ describe MessagesController do
     end
 
     it "assigns the requested contact to @message" do
-      get :index, params: {group_id: group.id}
+      get :index,params:{group_id:group.id}
       expect(assigns(:message)).to be_a_new(Message)
     end
 
     it "assigns the requested contact to @group" do
       @group = Group.find(group.id)
-      get :index, params: {group_id: group.id}
+      get :index,params:{group_id:group.id}
       expect(assigns(:group)).to eq @group
     end
 
     it "renders the :index template" do
-      get :index, params: { group_id: group.id}
+      get :index,params:{ group_id:group.id}
       expect(response).to render_template :index
     end
   end
