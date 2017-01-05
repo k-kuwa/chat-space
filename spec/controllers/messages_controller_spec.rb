e88adb757f208dc26db45a149038819634b805bc
k-kuwa <kyosuke@Kyosuke-no-MacBook-Pro.local>
2017-01-05 13:17:20 +0900

    testing create action

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe MessagesController do
 
-  describe 'GET #index' do
     let!(:user){create(:user)}
     let!(:group){create(:group)}
 
     before do
       sign_in user
     end
+
+  describe 'GET #index' do
 
     it "assigns the requested contact to @message" do
       get :index,params: {group_id: group.id}
@@ -26,4 +27,34 @@ describe MessagesController do
       expect(response).to render_template :index
     end
   end
+
+  describe "POST#create" do
+    let!(:message){build(:message)}
+
+   context "When body is present" do
+    it "save message" do
+      expect{
+        post :create, group_id: group.id,message: attributes_for(:message)
+      }.to change(Message, :count).by(1)
+    end
+
+    it "redirect to index" do
+      post :create, group_id: group.id, message: attributes_for(:message)
+      expect(response).to redirect_to group_messages_path
+    end
+   end
+
+   context "When there is no body." do
+    it "not save message" do
+      expect{
+        post :create, group_id: group.id,message: attributes_for(:message,body: nil)
+      }.not_to change(Message, :count)
+    end
+
+    it "redirect to index" do
+      post :create, group_id: group.id, message: attributes_for(:message,body: nil)
+      expect(response).to redirect_to group_messages_path
+    end
+   end
+ end
 end
