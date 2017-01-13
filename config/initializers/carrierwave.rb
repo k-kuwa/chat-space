53e85aa4ffd50c7a84aa97d497ff4f347b528898
k-kuwa <kyosuke@Kyosuke-no-MacBook-Pro.local>
2017-01-13 16:39:33 +0900

    Setting Product Environment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,7 +8,10 @@ CarrierWave.configure do |config|
   }
   case Rails.env
     when 'development'
-        config.fog_directory = 'chatspace-test'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-test'
+      config.fog_directory = 'chatspace-test'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-test'
+    when 'production'
+      config.fog_directory = 'chatspace-product'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-product'
   end
 end
